559d2b039913b936fa359e97ce1f6fc71074e6f2
Feynman Liang <feynman.liang@gmail.com>
2012-07-21 15:11:31 -0700

    Add vim temp to gitignore

--- app.rb
+++ app.rb
@@ -14,7 +14,7 @@ end
 configure :production do
     db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
     ActiveRecord::Base.establish_connection(
-        :adapter  => db.scheme == 'postgres' ? 'postgresql' : 'mysql2',
+        :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
         :host     => db.host,
         :port     => db.port,
         :username => db.user,

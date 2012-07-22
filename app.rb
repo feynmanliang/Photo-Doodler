757c75f27c06e3019517a40c3993f22eb0e3c38b
Eduardo Salinas <eduardosalinas@hotmail.com>
2012-07-22 00:35:26 -0700

    i dunno

--- app.rb
+++ app.rb
@@ -43,89 +43,120 @@ set :show_exceptions, false
 # permissions your app needs.
 # See https://developers.facebook.com/docs/reference/api/permissions/
 # for a full list of permissions
-FACEBOOK_SCOPE = 'user_likes,user_photos,user_photo_video_tags'
+FACEBOOK_SCOPE = 'user_likes,user_photos,user_photo_video_tags,publish_stream'
 
 unless ENV["FACEBOOK_APP_ID"] && ENV["FACEBOOK_SECRET"]
-  abort("missing env vars: please set FACEBOOK_APP_ID and FACEBOOK_SECRET with your app credentials")
+    abort("missing env vars: please set FACEBOOK_APP_ID and FACEBOOK_SECRET with your app credentials")
 end
 
 before do
-  # HTTPS redirect
-  if settings.environment == :production && request.scheme != 'https'
-    redirect "https://#{request.env['HTTP_HOST']}"
-  end
+    # HTTPS redirect
+    if settings.environment == :production && request.scheme != 'https'
+        redirect "https://#{request.env['HTTP_HOST']}"
+    end
 end
 
 helpers do
-  def host
-    request.env['HTTP_HOST']
-  end
+    def host
+        request.env['HTTP_HOST']
+    end
 
-  def scheme
-    request.scheme
-  end
+    def scheme
+        request.scheme
+    end
 
-  def url_no_scheme(path = '')
-    "//#{host}#{path}"
-  end
+    def url_no_scheme(path = '')
+        "//#{host}#{path}"
+    end
 
-  def url(path = '')
-    "#{scheme}://#{host}#{path}"
-  end
+    def url(path = '')
+        "#{scheme}://#{host}#{path}"
+    end
 
-  def authenticator
-    @authenticator ||= Koala::Facebook::OAuth.new(ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_SECRET"], url("/auth/facebook/callback"))
-  end
+    def authenticator
+        @authenticator ||= Koala::Facebook::OAuth.new(ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_SECRET"], url("/auth/facebook/callback"))
+    end
 
 end
 
 # the facebook session expired! reset ours and restart the process
 error(Koala::Facebook::APIError) do
-  session[:access_token] = nil
+    session[:access_token] = nil
 
-  redirect "/auth/facebook"
+    redirect "/auth/facebook"
+end
+
+get "/post" do
+    # Get base API Connection
+    @graph  = Koala::Facebook::API.new(session[:access_token])
+
+    # Get public details of current application
+    @app  =  @graph.get_object(ENV["FACEBOOK_APP_ID"])
+    if session[:access_token]
+        @graph.put_connections("me", "links", {:name => "le goog", :link => "www.google.com", :picture => "http://lh4.googleusercontent.com/-v0soe-ievYE/AAAAAAAAAAI/AAAAAAAAdPc/JRVJ5ihOy2U/photo.jpg?sz=116"});
+    end
+    "asdf"
+end
+
+get "/list" do
+    # Get base API Connection
+    @graph  = Koala::Facebook::API.new(session[:access_token])
+
+    # Get public details of current application
+    @app  =  @graph.get_object(ENV["FACEBOOK_APP_ID"])
+
+    if session[:access_token]
+        fake_graph_collection = Koala::Facebook::GraphCollection.new({"data" => []}, @graph)
+    # use GraphCollection's method to get Koala-friendly params from the Facebook URL
+    url_params = fake_graph_collection.parse_page_url(params[:page])
+    # then use those params to get the next page
+    @results = @graph.get_page(url_params)
+
+     #   @results = params[:page] ? @graph.get_page(params[:page]) : @graph.get_connections("me", "photos")
+        erb :list_gen
+    end
 end
 
 get "/" do
-  # Get base API Connection
-  @graph  = Koala::Facebook::API.new(session[:access_token])
+    # Get base API Connection
+    @graph  = Koala::Facebook::API.new(session[:access_token])
 
-  # Get public details of current application
-  @app  =  @graph.get_object(ENV["FACEBOOK_APP_ID"])
+    # Get public details of current application
+    @app  =  @graph.get_object(ENV["FACEBOOK_APP_ID"])
 
-  if session[:access_token]
-    @user    = @graph.get_object("me")
-    @friends = @graph.get_connections('me', 'friends')
-    @photos  = @graph.get_connections('me', 'photos').first(24)
-    @likes   = @graph.get_connections('me', 'likes').first(4)
+    if session[:access_token]
+        @user    = @graph.get_object("me")
+        @friends = @graph.get_connections('me', 'friends')
+        @photos  = @graph.get_connections('me', 'photos')
+        @likes   = @graph.get_connections('me', 'likes').first(4)
 
-    # for other data you can always run fql
-    @friends_using_app = @graph.fql_query("SELECT uid, name, is_app_user, pic_square FROM user WHERE uid in (SELECT uid2 FROM friend WHERE uid1 = me()) AND is_app_user = 1")
-  end
-  erb :index
+        # for other data you can always run fql
+        @friends_using_app = @graph.fql_query("SELECT uid, name, is_app_user, pic_square FROM user WHERE uid in (SELECT uid2 FROM friend WHERE uid1 = me()) AND is_app_user = 1")
+    end
+    erb :index
 end
 
 # used by Canvas apps - redirect the POST to be a regular GET
 post "/" do
-  redirect "/"
+    redirect "/"
 end
 
 # used to close the browser window opened to post to wall/send to friends
 get "/close" do
-  "<body onload='window.close();'/>"
+    "<body onload='window.close();'/>"
 end
 
 get "/sign_out" do
-  session[:access_token] = nil
-  redirect '/'
+    session[:access_token] = nil
+    redirect '/'
 end
 
 get "/auth/facebook" do
-  session[:access_token] = nil
-  redirect authenticator.url_for_oauth_code(:permissions => FACEBOOK_SCOPE)
+    session[:access_token] = nil
+    redirect authenticator.url_for_oauth_code(:permissions => FACEBOOK_SCOPE)
 end
 
 get '/auth/facebook/callback' do
-	session[:access_token] = authenticator.get_access_token(params[:code])
-	redirect '/'
+    session[:access_token] = authenticator.get_access_token(params[:code])
+    redirect '/'
 end
